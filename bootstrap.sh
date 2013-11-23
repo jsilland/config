f00137d0faf133cf38fa0c33db78fff1bf068560
Julien Silland <julien@soliton.io>
2013-11-23 08:44:40 -0800

    Documents the bootstrap script

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+# Contains the bootstraping code for runnning the setup script.
+# This essentially clones this repository in ~/src – the first invocation of git
+# will prompt to install the Xcode command line tools and sign you soul to
+# the devil
+
 mkdir ~/src
 git clone https://github.com/jsilland/config ~/src/config
 ~/src/config/setup.sh
