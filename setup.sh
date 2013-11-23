06bb31484121d166e1dba08568f2d916164bb0eb
Julien Silland <julien@soliton.io>
2013-11-23 08:32:59 -0800

    Adding SourceSansPro

--- setup.sh
+++ setup.sh
@@ -33,6 +33,9 @@ git clone https://github.com/chriskempson/tomorrow-theme ~/src/tomorrow-theme
 curl -f -o ~/Downloads/SourceCode.zip "http://softlayer-dal.dl.sourceforge.net/project/sourcecodepro.adobe/SourceCodePro_FontsOnly-1.017.zip"
 unzip ~/Downloads/SourceCode.zip -d ~/Downloads/
 cp ~/Downloads/SourceCodePro_FontsOnly-1.017/OTF/*.otf ~/Library/Fonts/
+curl -L -v -o ~/Downloads/SourceSansPro_FontsOnly-1.050.zip "http://sourceforge.net/projects/sourcesans.adobe/files/SourceSansPro_FontsOnly-1.050.zip/download"
+unzip ~/Downloads/SourceSansPro_FontsOnly-1.050.zip -C ~/Downloads/
+cp ~/Downloads/SourceSansPro_FontsOnly-1.050/OTF/*.otf ~/Library/Fonts/
 
 # Vim
 ln -s ~/src/config/dotfiles/vimrc ~/.vimrc
