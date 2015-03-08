a0207360feda95fb426cd1c56060f74b35b6eb11
Julien Silland <julien@soliton.io>
2015-03-08 15:45:00 -0700

    Changes download location for SourceSans

--- source_font.sh
+++ source_font.sh
@@ -5,7 +5,8 @@
 curl -f -o ~/Downloads/SourceCode.zip "http://softlayer-dal.dl.sourceforge.net/project/sourcecodepro.adobe/SourceCodePro_FontsOnly-1.017.zip"
 unzip ~/Downloads/SourceCode.zip -d ~/Downloads/
 cp ~/Downloads/SourceCodePro_FontsOnly-1.017/OTF/*.otf ~/Library/Fonts/
-curl -L -v -o ~/Downloads/SourceSansPro_FontsOnly-1.050.zip "http://sourceforge.net/projects/sourcesans.adobe/files/SourceSansPro_FontsOnly-1.050.zip/download"
-unzip ~/Downloads/SourceSansPro_FontsOnly-1.050.zip -C ~/Downloads/
-cp ~/Downloads/SourceSansPro_FontsOnly-1.050/OTF/*.otf ~/Library/Fonts/
+
+curl -L -v -o ~/Downloads/SourceSansPro.zip "https://github.com/adobe-fonts/source-sans-pro/archive/2.010R-ro/1.065R-it.zip"
+unzip ~/Downloads/SourceSansPro.zip -d ~/Downloads/
+cp ~/Downloads/source-sans-pro-2.010R-ro-1.065R-it/OTF/*.otf ~/Library/Fonts
 
