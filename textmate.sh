240cedc23fc5e6da77e2e11b37111dbbbbe08b51
Julien Silland <julien@soliton.io>
2015-03-08 16:05:03 -0700

    Fixes invocation of tar in Textmate install

--- textmate.sh
+++ textmate.sh
@@ -3,4 +3,4 @@
 # Dwonloads and installs Textmate
 
 curl -L -o ~/Downloads/TextMate_2.0-beta.1.tbz "https://github.com/textmate/textmate/releases/download/v2.0-beta.1/TextMate_2.0-beta.1.tbz"
-tar -xjf ~/Downloads/TextMate_2.0-beta.1.tbz /Applications/
+tar -xjf ~/Downloads/TextMate_2.0-beta.1.tbz -C /Applications/
