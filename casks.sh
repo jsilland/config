132ff6351a4c7e9dfafe1362d696fddfee8604e0
Julien Silland <julien@soliton.io>
2015-07-29 16:53:44 -0700

    Vim setup

--- casks.sh
+++ casks.sh
@@ -3,6 +3,12 @@
 brew cask install google-chrome
 brew cask install vlc
 brew cask install textmate
+
+mkdir ~/.vim
+ln -s ~/src/tomorrow-theme/vim/colors ~/.vim/colors
+ln -s ~/src/config/dotfiles/vimrc ~/.vimrc    ln -s ~/src/config/dotfiles/vimrc ~/.vimrc
+cp Applications/MacVim.app/../mvim /usr/local/bin/gvim
+
 brew cask install macvim
 brew cask install intellij-idea-ce
 brew cask install rubymine
